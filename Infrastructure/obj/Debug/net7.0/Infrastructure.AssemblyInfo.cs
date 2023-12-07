5aa93b4d7ed10bfb9ef6d2f782435db3379c82ea
Mika <mika-roos@web.de>
2023-12-07 22:13:43 +0100

    - sln. Configuration

--- Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d325448f6f11aca7560c10b1b9c03720c97fcf43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
